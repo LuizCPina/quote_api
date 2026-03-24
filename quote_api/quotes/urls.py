7fccb13666e95fdaecb96883fd6ee2666b05e0f5
Luiz Henrique <luizricopina@hotmail.com>
2026-03-24 16:07:20 -0300

    add funcionalidade adicionar frase

--- quote_api/quotes/urls.py
+++ quote_api/quotes/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import home,QuoteDetailView, QuoteListCreate, QuoteRandomView
+from .views import home,QuoteDetailView, quote_list_create, QuoteRandomView
 
 urlpatterns = [    
     path('', home, name='home'),
-    path('quotes/', QuoteListCreate.as_view(), name='quote-list-create'),
+    path('quotes/', quote_list_create, name='quote-list-create'),
     path('quotes/random/', QuoteRandomView.as_view(), name='quote-random'),
     path('quotes/<int:id>/', QuoteDetailView.as_view(), name='quote-detail'),
 ]
--- quote_api/quotes/views.py
+++ quote_api/quotes/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+import json
 from .serializers import QuoteSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Quote
+from django.views.decorators.csrf import csrf_exempt
 
-class QuoteListCreate(APIView):
 
-    def get(self, request):
+@csrf_exempt
+def quote_list_create(request):
+    if request.method == 'GET':
         quotes = Quote.objects.all()
         serializer = QuoteSerializer(quotes, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
-    
-    def post(self, request):
-        serializer = QuoteSerializer(data=request.data)
+        return JsonResponse(serializer.data, safe=False, status=200)
 
+    elif request.method == 'POST':
+        data = json.loads(request.body)
+        serializer = QuoteSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status.HTTP_201_CREATED)     
-        
-        else:
-            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST) 
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
 class QuoteRandomView(APIView):
 
@@ -32,8 +34,7 @@ class QuoteRandomView(APIView):
             serializer = QuoteSerializer(quote)
             return Response(serializer.data, status.HTTP_200_OK)
         
-        else:
-            return Response({'error': 'No quotes available'}, status.HTTP_404_NOT_FOUND)
+        return Response({'error': 'No quotes available'}, status.HTTP_404_NOT_FOUND)
         
 
 class QuoteDetailView(APIView):
