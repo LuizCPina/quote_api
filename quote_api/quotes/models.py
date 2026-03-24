867a674660f2990dc6db1df17e943df561b4023c
Luiz Henrique <luizricopina@hotmail.com>
2026-03-24 10:21:18 -0300

    create quote model

--- quote_api/quotes/models.py
+++ quote_api/quotes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = [
+    ('motivacional', 'Motivacional'),
+    ('reflexiva', 'Reflexiva'),
+    ('humor', 'Humor'),
+    ('triste','Triste'),
+    ]
+
+class Quote(models.Model):
+    text = models.TextField()
+    author = models.CharField(max_length=255,null=True, blank=True)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='motivacional')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.author:
+            return f'"{self.text}" - {self.author} ({self.category})'
+        else:
+            return f'"{self.text}" - {self.category}'
